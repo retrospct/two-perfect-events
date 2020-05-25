e50e809e6120bf8c3b675f3fc9a83fb4e54b14a0
Justin Lee <leejustin@vmware.com>
2020-05-24 18:28:16 -0700

    fix to SEO

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { SEO } from 'components/common/SEO'
 import { Link } from 'gatsby'
 import { Layout, Seo } from 'components/common'
 // import { SEO } from '../components/common/SEO'
