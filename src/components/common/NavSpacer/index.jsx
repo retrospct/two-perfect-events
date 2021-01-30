ef6351f409c43171e0307b7bc0c7beeb4303f5cd
Justin Lee <leejustin@vmware.com>
2021-01-30 12:21:26 -0800

    Fixes header padding spacing

--- src/components/common/NavSpacer/index.jsx
+++ src/components/common/NavSpacer/index.jsx
@@ -6,8 +6,8 @@ export const NavSpacer = ({ children }) => <MainContent>{children}</MainContent>
 const MainContent = styled.div`
   height: 100%;
   width: 100%;
-  padding-top: 88px;
-  @media (max-width: ${({ theme }) => `${theme.mq.lg}px`}) {
+  /* padding-top: 88px; */
+  /* @media (max-width: ${({ theme }) => `${theme.mq.lg}px`}) {
     padding-top: 69px;
-  }
+  } */
 `
